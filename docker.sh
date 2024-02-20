5d77b12f77dc1a7588ca3cc8ac4a2fb3219033df
FarouHub <sittler.n@gmail.com>
2024-02-20 15:18:54 +0100

    add latest

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Copy necessary files from baqmen/cicddocker image to code-test volume
-docker run --rm -v code-test:/var/www/project baqmen/cicddocker cp -r /var/www/project/* /var/www/project/
+docker run --rm -v code-test:/var/www/project baqmen/cicddocker:latest cp -r /var/www/project/* /var/www/project/
